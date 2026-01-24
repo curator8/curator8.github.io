9e5530a503973b31edadaf7a2a46243ea18c5d7b
curator8 <montanojoel68@gmail.com>
2026-01-23 21:22:07 -0500

    adding cursor logic

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-function print_hello() {
-    return "hello world"
+function AddAnimatedCircle() {
+  const cursor = document.getElementById("cursorId");
+
+  document.addEventListener("mousemove", (e) => {
+    cursor.style.left = `${e.clientX}px`;
+    cursor.style.top = `${e.clientX}px`;
+  });
+
+  document.addEventListener("click", () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+      cursor.classList.remove("expand");
+    }, 500);
+  });
 }
 
-console.log(print_hello())
+AddAnimatedCircle();
